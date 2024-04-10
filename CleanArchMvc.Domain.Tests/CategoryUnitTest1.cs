bd187eb2835760789b1c5e053b680feabff733e2
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-04-10 19:53:04 -0300

    create test domain

---
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+
+namespace CleanArchMvc.Domain.Tests;
+
+public class CategoryUnitTest1 {
+    [Fact]
+    public void CreateCategory_WithValidParameters_ResultObjectValidState() {
+        Action action = () => {
+            var category = new Category(1, "Category Name ");
+        };
+        action.Should()
+            .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+    }
+}
