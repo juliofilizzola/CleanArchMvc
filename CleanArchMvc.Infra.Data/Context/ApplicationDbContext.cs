e29938387821ef6dd69f636680cce256598422af
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-04-13 20:32:57 -0300

    create application db context

---
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Context;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options) {
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product>  Products   { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder) {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
