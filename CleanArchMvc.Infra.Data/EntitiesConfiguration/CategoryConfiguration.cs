f155ebb1c1fdeb7fbb0e01b8cf33cbffb8543717
Julio Filizzola <juliofilizzola@Hotmail.com>
2024-04-13 20:54:26 -0300

    Create configuration

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category> {
+    public void Configure(EntityTypeBuilder<Category> builder) {
+        builder.HasKey(t => t.Id);
+        builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+
+        builder.HasData(
+            new Category(1, "Eletro"),
+            new Category(2, "Acessorios"),
+            new Category(3, "MAT. Escolar")
+        );
+    }
+}
