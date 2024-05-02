e27ccac7ba6cfee9dc31d46a53306ccb89c447a2
Julio Filizzola <juliofilizzola@hotmail.com>
2024-05-02 16:19:03 -0300

    create product service interface

---
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchMvc.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task <ProductDTO> GetProductById(int? id);
+        Task <ProductDTO> Add(ProductDTO productDTO);
+        Task<ProductDTO> Update(ProductDTO productDTO);
+        Task<Boolean> Remove(int? id);
+    }
+}
