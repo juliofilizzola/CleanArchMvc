a54e3b19ca874c4368629958ebf57e0c9536d26f
Julio Filizzola <juliofilizzola@hotmail.com>
2024-05-02 13:28:50 -0300

    generate mapping profile

---
+++ CleanArchMvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTO;
+using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile() {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
