628471fecd75d607fde9f976ba3caeff9f661e1a
Julio Filizzola <juliofilizzola@hotmail.com>
2024-05-02 13:25:20 -0300

    generate mapping

---
+++ CleanArchMvc.Application/DTO/CategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.DTO
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
