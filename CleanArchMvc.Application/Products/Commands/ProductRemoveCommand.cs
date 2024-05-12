dbf5b65372bc4654680cdb4ced92577381a4d8b1
julio_filizzola <juliofilizzola@hotmail.com>
2024-05-12 16:56:46 -0300

    create products commands

---
+++ CleanArchMvc.Application/Products/Commands/ProductRemoveCommand.cs
@@ -0,0 +1,8 @@
+﻿using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Commands;
+
+public class ProductRemoveCommand(int id) : IRequest<Product> {
+    public int Id { get; set; } = id;
+}
