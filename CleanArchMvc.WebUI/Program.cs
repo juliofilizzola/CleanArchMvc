654ef24041dc3e7fa4b9924cc0cd55ce9e6b7810
julio_filizzola <juliofilizzola@hotmail.com>
2024-05-12 15:52:00 -0300

    create view categories from web ui

--- CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc.WebUI/Program.cs
@@ -26,6 +26,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Categories}/{action=Index}/{id?}"
+);
 
 app.Run();
