1339791e4d5147268f7344960b4e548f3ab5f10e
julio_filizzola <juliofilizzola@hotmail.com>
2024-05-26 08:27:46 -0300

    create Home index

---
+++ CleanArchMvc.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers;
+
+public class HomeController : Controller {
+    // GET
+    public IActionResult Index() {
+        return View();
+    }
+}
---
+++ CleanArchMvc.WebUI/Views/Home/Index.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CleanArchMvc.WebUI.Views.Home;
+
+public class Index : PageModel {
+    public void OnGet() {
+        
+    }
+}
