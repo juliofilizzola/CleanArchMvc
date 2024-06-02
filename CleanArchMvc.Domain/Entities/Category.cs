b3ed6a98127a8cfbbc6d1bc072ffeaa5e2f57f7d
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-02 20:46:16 -0300

    Add Base from category Entities

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -2,36 +2,36 @@
 
 namespace CleanArchMvc.Domain.Entities;
 
-public sealed class Category {
-    public int Id { get; private set; }
-    public string   Name  { get; private set; }
-    public ICollection<Product> Products { get; private set; }
+public sealed class Category : Base {
 
-    public Category(string name) {
-        ValidateDomainName(name);
+    public string Name { get; private set; }
+
+    public Category(string name)
+    {
+        ValidateDomain(name);
+    }
+
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+        Id = id;
+        ValidateDomain(name);
+    }
+
+    public void Update(string name)
+    {
+        ValidateDomain(name);
+    }
+    public ICollection<Product> Products { get; set; }
+
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+            "Invalid name.Name is required");
+
+        DomainExceptionValidation.When(name.Length < 3,
+            "Invalid name, too short, minimum 3 characters");
+
         Name = name;
-    }
-
-    public Category(int id, string name) {
-        ValidateDomainName(name);
-        ValidateDomainId(id);
-
-        Id   = id;
-        Name = name;
-    }
-
-    public void Update(String name) {
-        ValidateDomainName(name);
-        Name = name;
-    }
-
-    private void ValidateDomainName(string name) {
-        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is Required");
-
-        DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
-    }
-
-    private void ValidateDomainId(int id) {
-        DomainExceptionValidation.When(id < 0, "Invalid id value");
     }
 }
