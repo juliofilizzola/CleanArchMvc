f7dbd95e0a5b9da453adc952776749aadad1bc92
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-02 08:26:01 -0300

    remove files

--- CleanArchMvc.WebUI/Views/Privacy/Index.cshtml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace CleanArchMvc.WebUI.Views.Privacy;
-
-public class Index : PageModel {
-    public void OnGet() {
-        
-    }
-}
--- CleanArchMvc.WebUI/Views/Products/Index.cshtml.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace CleanArchMvc.WebUI.Controllers.Products;
-
-public class Index : PageModel {
-    public void OnGet() {
-        
-    }
-}
