022ac09380d71d710cff7a7548f4b84f053246e9
julio_filizzola <juliofilizzola@hotmail.com>
2024-06-02 08:38:45 -0300

    update and create error system

--- CleanArchMvc.WebUI/Controllers/HomeController.cs
+++ CleanArchMvc.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CleanArchMvc.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMvc.WebUI.Controllers;
 
@@ -6,5 +7,27 @@ public class HomeController : Controller {
     // GET
     public IActionResult Index() {
         return View();
+    }
+
+
+    [Route("Home/Error")]
+    public IActionResult Error(int statusCode)
+    {
+        var viewModel = new ErrorViewModel { StatusCode = statusCode };
+
+        switch (statusCode)
+        {
+            case 404:
+                viewModel.Message = "Página não encontrada";
+                break;
+            case 500:
+                viewModel.Message = "Erro interno do servidor";
+                break;
+            default:
+                viewModel.Message = "Ocorreu um erro inesperado";
+                break;
+        }
+
+        return View(viewModel);
     }
 }
